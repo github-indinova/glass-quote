b63314afdd94964eec9523667c1e9a2fd24a43cb
github-indinova <krishan@indinovatechnologies.com>
2013-07-12 23:10:21 +0530

    added new form

--- glass-quote/FormMain.Designer.cs
+++ glass-quote/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace glass_quote
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -30,12 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Glass Depots USA - Quote Screen";
             this.ResumeLayout(false);
 
--- glass-quote/Program.cs
+++ glass-quote/Program.cs
@@ -15,7 +15,7 @@ namespace glass_quote
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
